5788c8cf9a09052d602be457afe36dc465e608d8
Adsuarez <adsuarezq@gmail.com>
2023-04-10 17:41:40 -0500

    update returned value to view codigo besides nombre on admin page

--- crud/api/models.py
+++ crud/api/models.py
@@ -6,15 +6,17 @@ class Departamento(models.Model):
     presupuesto = models.BigIntegerField()
 
     def __str__(self):
-        return self.nombre
+        return f"{self.codigo}"
+    
+    
     
 class Empleado(models.Model):
     codigo = models.SmallIntegerField(primary_key=True)
     nif = models.CharField(max_length=9)
     nombre = models.CharField(max_length=100)
     apellido1 = models.CharField(max_length=100)
-    apellido2 = models.CharField(max_length=100)
+    apellido2 = models.CharField(max_length=100, default="")
     codigo_departamento = models.ForeignKey(Departamento, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.codigo}"
