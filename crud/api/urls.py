991377a966291c341cbd72e27cb2fc2599b53700
Adsuarez <adsuarezq@gmail.com>
2023-04-12 12:17:35 -0500

    create utl to get single employee

--- crud/api/urls.py
+++ crud/api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('departamentos/', views.getAllDepartments),
     path('departamentos/crear/', views.createDepartment),
     path('empleados/', views.getAllEmployees),
+    path('empleados/<int:code>/', views.getEmployee),
     path('empleados/crear/', views.createEmployee),
     path('empleados/actualizar/<int:code>/', views.updateEmployee),
     path('empleados/eliminar/<int:code>/', views.deleteEmployee),
