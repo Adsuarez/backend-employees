c333f41785c734c2cbe0597c707b20cd795b3010
Adsuarez <adsuarezq@gmail.com>
2023-04-14 15:05:24 -0500

    add variables and commands to read environment variables

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -14,10 +14,14 @@ from pathlib import Path
 import environ
 
 env = environ.Env(
+    # set casting, default value
     DEBUG=(bool, False)
 )
-#reading .env file
+# reading .env file
 environ.Env.read_env()
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,9 +32,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
